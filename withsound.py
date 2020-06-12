82104044b21660c37225addb1d8631a9eaf4e9e9
Sandy718 <52973590+Sandy718@users.noreply.github.com>
2020-06-12 12:16:39 +0530

    final edit

--- withsound.py
+++ withsound.py
@@ -26,7 +26,7 @@ CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
            "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
            "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
            "sofa", "train", "tvmonitor"]
-IGNORE = set(["sheep", "background", "aeroplanqe", "boat", "horse"])
+IGNORE = set(["sheep", "background", "aeroplane", "boat", "horse"])
 COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
 
 # load our serialized model from disk
